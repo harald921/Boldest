f940664488707ce4ea5e2d3f61dcca277206ebb3
Unknown <johan10000@outlook.com>
2017-05-22 17:22:19 +0200

    add cool cube

---
+++ Boldest/Assets/Scripts/Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+
+    void Update()
+    {
+
+        transform.Rotate(new Vector3(5, 10, 5));
+
+    }
+       
+        
+}
