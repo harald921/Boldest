8b7782ad49618bd0948ac7eef6ea641e4b44b140
Unknown <harald921@hotmail.com>
2017-05-23 17:37:31 +0200

    Implement basic movement and aiming

---
+++ Boldest/Assets/Code/FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    GameObject _playerGO;
+    Vector3 _posOffset;
+
+    void Start()
+    {
+        _playerGO = GameObject.Find("Player");
+        _posOffset = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position =  new Vector3((_playerGO.transform.position + _posOffset).x, transform.position.y, (_playerGO.transform.position + _posOffset).z);
+    }
+}
---
+++ Boldest/Assets/Code/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float _health      = 100.0f;
+    [SerializeField] float _moveSpeed   = 16.0f;
+    [SerializeField] float _turnSpeed   = 1.0f;
+
+    Vector3 _movementVector = Vector3.zero;
+
+    private void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        HandleMovement();
+        HandleAiming();
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddForce(_movementVector.normalized * _moveSpeed);
+
+        _movementVector = Vector3.zero;
+    }
+
+    void HandleMovement()
+    {
+        _movementVector = Vector3.zero;
+
+        if      (Input.GetKey(KeyCode.W)) _movementVector += Vector3.forward;
+        else if (Input.GetKey(KeyCode.S)) _movementVector += Vector3.back;
+        if      (Input.GetKey(KeyCode.A)) _movementVector += Vector3.left;
+        else if (Input.GetKey(KeyCode.D)) _movementVector += Vector3.right;
+    }
+
+    void HandleAiming()
+    {
+        Vector3 aimTarget;
+
+        aimTarget = Input.mousePosition;
+        aimTarget.z = Mathf.Abs(Camera.main.transform.position.y - transform.position.y);
+        aimTarget = Camera.main.ScreenToWorldPoint(aimTarget);
+        aimTarget = new Vector3(aimTarget.x, transform.position.y, aimTarget.z);
+
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(aimTarget - transform.position), _turnSpeed * Time.deltaTime);
+    }
+}
