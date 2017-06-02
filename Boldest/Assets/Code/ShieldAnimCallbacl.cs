cff091843adfb8b75e191ac6b5104409a9fb8f51
Johan <johan10000@outlook.com>
2017-06-02 18:14:56 +0200

    add a guy with a shield

---
+++ Boldest/Assets/Code/ShieldAnimCallbacl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldAnimCallbacl : MonoBehaviour
+{
+
+
+    void AnimationDone()
+    {
+        GetComponent<Animator>().SetBool("Attack", false);
+    }
+
+    
+}
