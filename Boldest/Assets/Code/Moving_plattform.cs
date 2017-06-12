2b4137b57f36d6dc94b0d90dbc725cf9d8518f8b
Unknown <rickardrl@gmail.com>
2017-06-12 14:29:04 +0200

    Added moving platform

---
+++ Boldest/Assets/Code/Moving_plattform.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving_plattform : MonoBehaviour {
+
+    public GameObject[] _positions;
+    public float speed = 1.0f;
+    public int _startPos;
+    int _currentPosition = 0;
+    [HideInInspector]
+    public bool _move = true;
+
+    void Update()
+    {
+        if (_move)
+        {
+            transform.GetChild(0).position = Vector3.MoveTowards(transform.GetChild(0).position, _positions[_currentPosition].transform.position, speed * Time.deltaTime);
+
+            Vector3 forward = _positions[_currentPosition].transform.position - transform.GetChild(0).position;
+            forward.Normalize();
+
+        }
+
+        if (transform.GetChild(0).position == _positions[_currentPosition].transform.position)
+        {
+            if (_currentPosition < _positions.Length - 1)
+                _currentPosition++;
+            else
+                _currentPosition = 0;
+
+
+        }
+
+
+
+    }
+
+    
+}
