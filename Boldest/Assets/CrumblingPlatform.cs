6b3baa2f7ea21ddfae3ab80eb9247e60f39dbd30
Unknown <harald921@hotmail.com>
2017-06-15 18:00:58 +0200

    Add crumbling platforms

--- Boldest/Assets/CrumblingPlatform.cs
+++ Boldest/Assets/CrumblingPlatform.cs
@@ -5,13 +5,48 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class CrumblingPlatform : MonoBehaviour
 {
-
     [SerializeField] float _timeBeforeCrumble = 2.0f;
+
+    [SerializeField] float _blinkInterval = 0.1f;
+
+    Color _startColor;
+
+    private void Awake()
+    {
+        _startColor = GetComponent<MeshRenderer>().material.color;
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(Blink(_timeBeforeCrumble));
             StartCoroutine(Crumble());
+        }
+    }
+
+    IEnumerator Blink(float inTimeToBlink)
+    {
+        bool colorToggle = true;
+        float blinkTimer = 0;
+
+        while (blinkTimer <= inTimeToBlink)
+        {
+            blinkTimer += _blinkInterval;
+
+            Debug.Log(blinkTimer);
+
+            if (colorToggle)
+                GetComponent<MeshRenderer>().material.color = Color.gray;
+            else
+                GetComponent<MeshRenderer>().material.color = _startColor;
+
+            colorToggle = !colorToggle;
+
+            yield return new WaitForSeconds(_blinkInterval);
+        }
+
+        GetComponent<MeshRenderer>().material.color = Color.gray;
     }
 
     IEnumerator Crumble()
