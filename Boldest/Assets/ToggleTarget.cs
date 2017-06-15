591aa897e586100da11621d160a1a145af82a415
Unknown <harald921@hotmail.com>
2017-06-15 14:15:36 +0200

    Add togglable floors

---
+++ Boldest/Assets/ToggleTarget.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleTarget : MonoBehaviour
+{
+    public bool _isActive = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Arrow")
+        {
+            _isActive = !_isActive;
+        }
+    }
+}
