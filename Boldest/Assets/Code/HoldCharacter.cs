14055ce60f1d14ff98fb83756d96e3c4f322b88b
Unknown <rickardrl@gmail.com>
2017-06-15 16:07:35 +0200

    ADDED MOVING PLATFORMS BITCHES WOHO WE ARE IN THIS!

---
+++ Boldest/Assets/Code/HoldCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldCharacter : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+            col.transform.parent = gameObject.transform;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+            col.transform.parent = null;
+    }
+}
